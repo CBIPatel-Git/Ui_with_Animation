7fb52eef678460d42bf8c87d79f53b061d871a60
CBIPatel-Git <chintanitaliya0010@gmail.com>
2021-10-16 16:02:55 +0530

    Final Commit

--- lib/travel/travel_page.dart
+++ lib/travel/travel_page.dart
@@ -10,7 +10,7 @@ class TravelPage extends StatefulWidget {
 
 class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
   final img_url =
-      "http://www.happywalagift.com/wp-content/uploads/2015/08/Indian-Flag-Wallpapers-HD-Images-Free-Download.jpg";
+      "https://1.bp.blogspot.com/-ghNGqcGTzDg/WIw9-tnmdqI/AAAAAAAAKEA/pXEG4wn4Dcw92_bvJc86fiixPA3NAzLGgCLcB/s1600/Indian%2BFlag%2BWallpapers.jpg";
 
   final pic1 =
       "https://www.unciatrails.com/wp-content/uploads/2020/07/Taj2.jpg";
@@ -18,15 +18,37 @@ class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
       "https://i.pinimg.com/originals/e2/a7/1f/e2a71f9b3c3bb0c1ba90e268b4ae90be.jpg";
 
   late TabController _tabController;
+  double anim = 1.0;
+  double anim2 = 1.0;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 4, vsync: this);
+
+    withAnimation(
+      vsync: this,
+      tween: Tween(begin: 1.0, end: 0.0),
+      callBack: (animationVal, controllerVal) {
+        anim = double.tryParse(animationVal.toString())!;
+        setState(() {});
+      },
+    );
+
+    withRepeatAnimation(
+      vsync: this,
+      isRepeatReversed: true,
+      tween: Tween(begin: 2.0, end: 3.0),
+      callBack: (animationVal, controllerVal) {
+        anim2 = double.tryParse(animationVal.toString())!;
+        setState(() {});
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    final tabIndex = _tabController.index;
     return Scaffold(
       backgroundColor: Vx.purple500,
       appBar: AppBar(
@@ -43,20 +65,23 @@ class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
       body: VStack([
         VxBox(
                 child: [
-          VxBox()
-              .square(100)
-              .roundedFull
-              .neumorphic(color: Vx.purple500, elevation: 30.0)
-              .bgImage(DecorationImage(image: NetworkImage(img_url)))
-              .make(),
+          Expanded(
+            child: VxBox()
+                .square(100)
+                .roundedFull
+                .neumorphic(color: Vx.purple500, elevation: 30.0)
+                .bgImage(DecorationImage(image: NetworkImage(img_url)))
+                .make(),
+          ),
           "Hi, "
               .richText
               .withTextSpanChildren(["India".textSpan.bold.make()])
               .white
               .xl2
               .make()
-              .p8(),
-          "Solo Traveller".text.white.make(),
+              .p8()
+              .offset(offset: Offset(-300 * anim, 0.0)),
+          "Solo Traveler".text.white.make(),
           VxTextField(
             borderType: VxTextFieldBorderType.none,
             borderRadius: 20,
@@ -93,28 +118,39 @@ class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
               unselectedLabelColor: Vx.gray400,
               labelPadding: Vx.m16,
               tabs: [
-                Icon(Icons.map, size: 30),
-                Icon(Icons.pin_drop, size: 30),
-                Icon(Icons.restaurant, size: 30),
-                Icon(Icons.person, size: 30),
+                Icon(Icons.map, size: 10)
+                    .scale(scaleValue: tabIndex == 0 ? anim2 : 3.00),
+                Icon(Icons.pin_drop, size: 10)
+                    .scale(scaleValue: tabIndex == 1 ? anim2 : 3.00),
+                Icon(Icons.restaurant, size: 10)
+                    .scale(scaleValue: tabIndex == 2 ? anim2 : 3.00),
+                Icon(Icons.person, size: 10)
+                    .scale(scaleValue: tabIndex == 3 ? anim2 : 3.00),
               ],
             ),
             TabBarView(
               controller: _tabController,
               children: ["1", "2", "3", "4"]
-                  .map((e) => VStack([
-                        "Discover Places in India".text.gray600.xl2.bold.make(),
-                        20.heightBox,
-                        TravelCard(
-                            title: "Taj Mahal",
-                            subttle: "Delhi",
-                            imageurl: pic1),
-                        20.heightBox,
-                        TravelCard(
-                            title: "Lonavala",
-                            subttle: "Maharashtra",
-                            imageurl: pic2)
-                      ]).p16())
+                  .map((e) => SingleChildScrollView(
+                        child: VStack([
+                          "Discover Places in India"
+                              .text
+                              .gray600
+                              .xl
+                              .bold
+                              .make(),
+                          15.heightBox,
+                          TravelCard(
+                              title: "Taj Mahal",
+                              subttle: "Delhi",
+                              imageurl: pic1),
+                          15.heightBox,
+                          TravelCard(
+                              title: "Lonavala",
+                              subttle: "Maharashtra",
+                              imageurl: pic2),
+                        ]).p16(),
+                      ))
                   .toList(),
             ).expand()
           ])).white.make(),
